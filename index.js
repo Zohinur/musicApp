1fd1145258d888b5e73b74d5d5fe910b300c58a6
Zohinur <zohinur@Zohinurs-MacBook-Air.local>
2025-07-14 14:26:42 -0400

    added new Property

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 const songs = [
     {
         src: "/audios/Afusic - Pal Pal with @Talwiinder  (Official Visualiser) Prod. @AliSoomroMusic.mp3",
-        img: "/images/ab67616d0000b273236d0c72"
+        img: "/images/ab67616d0000b273236d0c72c9c3cbbfa15dd208.jpeg",
+        title: "Pal Pal",
+        artist: "Afusic, Talwiinder"
 
     },
     { 
         src: "/audios/PARTYNEXTDOOR - Some Of Your Love (Lyrics) come give me some of your love.mp3",
-        img: "/images/maxresdefault.jpg"
+        img: "/images/maxresdefault.jpg",
+        title: "Some Of Your Love",
+        artist: "PARTYNEXTDOOR"
     },
     {
         src: "/audios/FloyyMenor, Anitta, Ozuna - Gata Only Remix (Audio Oficial).mp3",
         img: "/images/maxresdefault-2.jpg",
+        title: "Gata Only Remix",
+        artist: "FloyyMenor, Anitta, Ozuna"
     }
 ];
 
 let currentSongIndex = 0;
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const audioPlayer = document.getElementById('audioPlayer');
 const playBtn =document.getElementById('playBtn');
 const pauseBtn = document.getElementById('pauseBtn');
@@ -23,7 +31,7 @@ const prevBtn = document.getElementById('prevBtn');
 const songImg = document.getElementById('songImg');
 const progressBar = document.getElementById('progressBar');
 const currentTime = document.getElementById('currentTime');
-const durationTime = document.getElementById('duration');
+const durationTime = document.getElementById('durationTime');
 
 
 playBtn.addEventListener('click', () => {
@@ -52,6 +60,7 @@ nextBtn.addEventListener('click', () => {
     currentSongIndex = (currentSongIndex + 1) % songs.length;
     audioPlayer.src = songs[currentSongIndex].src;
     songImg.src = songs[currentSongIndex].img;
+    loadSong(currentSongIndex);
     audioPlayer.play();
 });
 
@@ -82,4 +91,18 @@ function formatTime(seconds) {
     const mins = Math.floor(seconds / 60) || 0;
     const secs = Math.floor(seconds % 60) || 0;
     return `${mins}:${secs < 10 ? '0' : ''}${secs}`;
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    audioPlayer.src = songs[currentSongIndex].src;
+    songImg.src = songs[currentSongIndex].img;
+    loadSong(currentSongIndex);
+});
+
+function loadSong(index) {
+    const song = songs[index];
+    audioPlayer.src = song.src;
+    songImg.src = song.img;
+    title.textContent = song.title;
+    artist.textContent = song.artist;
 }
